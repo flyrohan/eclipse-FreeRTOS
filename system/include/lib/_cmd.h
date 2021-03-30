3d3f7e828797d903d67c810261ef9b6f4b605fe4
jhkim <jhkim@coasia.com>
2021-03-30 13:25:28 +0900

    freertos: fix build warning
    Signed-off-by: jhkim <jhkim@coasia.com>

--- system/include/lib/_cmd.h
+++ system/include/lib/_cmd.h
@@ -14,12 +14,13 @@ typedef struct __CMD_Entry {
 int  CMD_Run(char *command);
 
 #if !defined(__ARMCC_VERSION)
+#define CMD_TYPE	static
 #define CMD_DEFINE(_cmd, _func) \
 	CMD_Entry _c_ent_##_func_##_cmd		\
 	__attribute__((unused, section(".cmd_entry_2_"#_cmd""))) = { #_cmd, _func }
 #else
 void CMD_Register(CMD_Entry *entry);
-
+#define CMD_TYPE
 #define CMD_DEFINE(_cmd, _func) \
 static CMD_Entry _c_ent_##_cmd	= { #_cmd, _func, LIST_HEAD_INIT(_c_ent_##_cmd.list) }; \
 void do_cmd_##_cmd(void) __attribute__((constructor(10)));	\
