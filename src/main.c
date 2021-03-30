aaff6f0fd1680df1349ac9a075d9831c10c88ba6
jhkim <jhkim@coasia.com>
2021-03-30 13:24:30 +0900

    freertos: support RTOS main app
    Signed-off-by: jhkim <jhkim@coasia.com>

--- src/main.c
+++ src/main.c
@@ -28,22 +28,41 @@
 // ----------------------------------------------------------------------------
 
 #include <config.h>
+#include <rtos.h>
 
 #define _MHZ(_hz)	(_hz/1000000)
 
-int main (void)
+#ifdef RTOS_ENABLED
+static void RunCLI(void)
+{
+	Printf("\r\n\r\n***** FREERTOS %ld MHZ *****\r\n\r\n", _MHZ(SYSTEM_CLOCK));
+	HAL_RTOSInit(SYSTEM_CLOCK/SYSTEM_TICK_HZ);
+#ifdef CLI_ENABLED
+	CLI_RunLoopThread(1024, osPriorityLow);
+#endif
+	vTaskStartScheduler();
+}
+#else
+static void RunCLI(void)
 {
 	Printf("\r\n\r\n***** FIRMWARE %ld MHZ *****\r\n\r\n", _MHZ(SYSTEM_CLOCK));
-#ifdef CLI_ENABLED
+
+	#ifdef CLI_ENABLED
 	CLI_RunLoop();
-#else
+	#else
 	#ifdef DMIPS_ENABLED
 	extern void DMIPS(int number_of_runs, int cpu_mhz);
 	do {
 		DMIPS(400000, _MHZ(SYSTEM_CLOCK));
 	} while (1);
 	#endif
+	#endif
+}
 #endif
+
+int main (void)
+{
+	RunCLI();
 
 	while (1) {
 		Printf("\r\n\r\n!!!!! EXIT FIRMWARE !!!!!\r\n\r\n")
