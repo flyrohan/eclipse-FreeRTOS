dac2ed5111351f3cfc1af18781bb2adc75d87f4e
jhkim <jhkim@coasia.com>
2021-03-30 13:22:35 +0900

    freertos: loop: support loop thread if enabled RTOS
    Signed-off-by: jhkim <jhkim@coasia.com>

--- src/loop.c
+++ src/loop.c
@@ -1,4 +1,7 @@
 #include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <rtos.h>
 #include <config.h>
 
 #ifdef LOOP_ENABLED
@@ -9,10 +12,64 @@
 		return 0;	\
 	} while (0)
 
-/*
- * $> loop <sleep:ms> <count>
- */
-int do_loop(int argc, char * const argv[])
+#ifdef RTOS_ENABLED
+
+static struct loop_t {
+	int ms, count;
+} _loop_t = {
+	.ms = 1000,
+	.count = 10,
+};
+
+static TaskHandle_t Loop_Handle = NULL;
+
+static void Loop_Thread(void *argument)
+{
+	struct loop_t *lt = (struct loop_t *)argument;
+	int count = 1;
+
+	do {
+		Printf("[%d] %d (%d ms), %d\r\n",
+				xTaskGetCurrentTaskHandle(), xTaskGetTickCount(), lt->ms, count);
+		if (lt->ms)
+			vTaskDelay((TickType_t)lt->ms);
+	} while (count++ < _loop_t.count);
+
+	Loop_Handle = NULL;
+	vTaskDelete(NULL);
+}
+
+CMD_TYPE int do_loop(int argc, char * const argv[])
+{
+	if (argc > 1 && argv[1]) {
+		if (!strcmp(argv[1], "exit")) {
+			if (Loop_Handle)
+				vTaskDelete(Loop_Handle);
+			Loop_Handle = NULL;
+			return 0;
+		}
+		_loop_t.ms = strtol(argv[1], NULL, 10);
+	}
+
+	if (argc > 2 && argv[2])
+		_loop_t.count = (int)strtol(argv[2], NULL, 10);
+
+	Printf("loop delay: %d ms, count %d (0x%x)\n", _loop_t.ms, _loop_t.count, Loop_Handle);
+
+	if (Loop_Handle)
+		vTaskDelete(Loop_Handle);
+
+	return xTaskCreate(Loop_Thread,
+			 	 	 "Loop-Task",
+					(( unsigned short )128),
+					(void *)&_loop_t,
+					(UBaseType_t)osPriorityLow,
+					&Loop_Handle);
+}
+
+#else
+
+CMD_TYPE int do_loop(int argc, char * const argv[])
 {
     int count = 10;
 	int ms = 0;
@@ -37,6 +94,10 @@ int do_loop(int argc, char * const argv[])
 
 	return 0;
 }
+#endif
 
+/*
+ * $> loop <sleep:ms> <count>
+ */
 CMD_DEFINE(loop, do_loop);
 #endif
